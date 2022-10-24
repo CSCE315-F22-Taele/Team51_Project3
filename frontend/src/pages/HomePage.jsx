71529a03f67bd97b5b249fa8216ac88fef4aaad3
Johnny Le <jwonnyleaf@gmail.com>
2022-10-23 20:17:45 -0500

    Initial

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div>
+      <header>
+        <nav className="navbar bg-light">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand mb-0 h1">
+              RevPOS
+            </Link>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <Link to="/pos" className="btn bt-primary">
+          Click Here !
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ frontend/src/pages/POSPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function POSPage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default POSPage

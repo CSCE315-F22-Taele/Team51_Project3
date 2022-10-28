9f0a79d193c4b28f675756586b4e1bb6cdb8ef64
Will Miranda <willm02@tamu.edu>
2022-10-28 16:06:56 -0500

    no more errors with npm start, but page empty and fetch not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import POSPage from "./pages/POSPage";
+import React, {useEffect, useState} from "react";
 
+
+const App = () => {
+  const [ingredient, setIngredients] = useState([]);
+  useEffect(() => {
+     fetch('http://localhost:3000/api/v1/pos')
+        .then((response) => response.json())
+        .then((response) => {console.log(response.body)})
+        .then((data) => {
+           console.log(data);
+           setIngredients(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+  }, []);
+
+   return (
+      <div className="ingredients-container">
+        {ingredient.map((ingredient) => {
+         return (
+               <div className="ingredient-id" key={ingredient.id}>
+               <h2 className="ingredient-name">{ingredient.name}</h2>
+               <h3 className="ingredient-price">{ingredient.price}</h3>
+               <h3 className="ingredient-calories">{ingredient.calories}</h3>
+               <h4 className="ingredient-inventory">{ingredient.inventory}</h4>
+               <div className="button">
+                  <div className="delete-btn">Delete</div>
+               </div>
+            </div>
+         );
+      })}
+   </div>
+   );
+}
+/*
 function App() {
   return (
     <Router>
@@ -12,12 +44,6 @@ function App() {
       </Routes>
     </Router>
   );
-  //THIS GIVES HEADACHE
-  /*fetch('http://localhost:3000/api/v1/pos')
-    .then((res) =>res.json())
-    .then(console.log);
-
-    return <div>Hello!</div>*/
-}
+}*/
 
 export default App;
--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import router from '../POS/routes';
+
 
 function POSPage() {
     return (

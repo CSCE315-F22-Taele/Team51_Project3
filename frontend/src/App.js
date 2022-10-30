8c0e9c6ba36a4a756c6036a3eba9773db9846f9b
Cuchador <willmiranda2002@gmail.com>
2022-10-30 11:41:18 -0500

    POSPage button formatting added, have to add functions to buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,50 @@
-import React, {useEffect, useState} from "react";
+import React from 'react';
+//import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+	from 'react-router-dom';
+import Home from './pages';
+import HomePage from './pages/HomePage';
+import POSPage from './pages/POSPage';
+import ManagerPage from './pages/ManagerPage';
 
 
-const App = () => {
-  const [ingredient, setIngredients] = useState([]);
-  useEffect(() => {
-     fetch('http://localhost:3000/api/v1/pos')
-        .then((response) => response.json())
-        .then((response) => {console.log(response.body)})
-        .then((data) => {
-           console.log(data);
-           setIngredients(data);
-        })
-        .catch((err) => {
-           console.log(err.message);
-        });
-  }, []);
-
-   return (
-      <div className="ingredients-container">
-        {ingredient.map((ingredient) => {
-         return (
-               <div className="ingredient-id" key={ingredient.id}>
-               <h2 className="ingredient-name">{ingredient.name}</h2>
-               <h3 className="ingredient-price">{ingredient.price}</h3>
-               <h3 className="ingredient-calories">{ingredient.calories}</h3>
-               <h4 className="ingredient-inventory">{ingredient.inventory}</h4>
-               <div className="button">
-                  <div className="delete-btn">Delete</div>
-               </div>
-            </div>
-         );
-      })}
-   </div>
-   );
-}
-/*
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/pos" element={<POSPage />} />
-      </Routes>
-    </Router>
-  );
-}*/
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route exact path='/' element={<Home />} />
+      <Route path='/HomePage' element={<HomePage />} />
+		<Route path='/POSPage' element={<POSPage/>} />
+		<Route path='/ManagerPage' element={<ManagerPage/>} />
+	</Routes>
+	</Router>
+);
+}
 
 export default App;
+
+
+/*function CreateMenuItemForm() {
+   const [name, setName] = useState('');
+
+   function handleSubmit(e) {
+      e.preventDefault();
+
+      fetch("http://localhost:3001/api/v1/pos", {
+         method: 'POST',
+         body: JSON.stringify({ name })
+      })
+   }
+   return (
+      <form onSubmit={handleSubmit}>
+         <input 
+         type="text" 
+         name="name" 
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+         />
+      </form>
+   )
+}*/

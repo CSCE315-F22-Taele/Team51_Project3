91bd3018dec3daa4f406377e819d91f9a6385428
Mohnish Pai <mohnishpai7@gmail.com>
2022-11-21 15:37:17 -0600

    Pair Report

--- backend/routes/pair.js
+++ backend/routes/pair.js
@@ -3,7 +3,10 @@ const controller = require('../controllers/pairController');
 
 const router = Router();
 
-router.get("/", controller.getPairs)
-router.get("/:firstDate/:secondDate", controller.getPairDates)
+// router.post("/", controller.createTable)
+router.patch("/", controller.insertPair)
+router.patch("/:firstDate/:secondDate", controller.insertPairDates)
+router.get("/", controller.getPair)
+// router.get("/:firstDate/:secondDate", controller.getPairDates)
 
 module.exports = router;

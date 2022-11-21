2e12b1525fad34b13d105b917888693db246f3a6
Johnny Le <jwonnyleaf@gmail.com>
2022-11-20 22:25:19 -0600

    Saving to branch

--- backend/controllers/excessController.js
+++ backend/controllers/excessController.js
@@ -1,23 +1,19 @@
 const pool = require("../server/db");
 const { response } = require("express");
 
-
 const getExcessDates = (req, res) => {
-
     const firstDate = req.params.firstDate;
     const secondDate = req.params.secondDate;
     pool.query(
-        "select * from daily_inventory where date between $1 and $2",
+        "SELECT * FROM daily_inventory WHERE date BETWEEN $1 and $2",
         [firstDate, secondDate],
         (error, results) => {
             if (error) throw error;
             res.status(200).send(results.rows);
         }
     );
-
 };
 
 module.exports = {
     getExcessDates,
-
 };
--- backend/routes/excess.js
+++ backend/routes/excess.js
@@ -1,8 +1,8 @@
-const { Router } = require('express');
-const controller = require('../controllers/excessController');
+const { Router } = require("express");
+const { getExcessDates } = require("../controllers/excessController");
 
 const router = Router();
 
-router.get("/:firstDate/:secondDate", controller.getExcessDates)
+router.get("/:firstDate/:secondDate", getExcessDates);
 
 module.exports = router;

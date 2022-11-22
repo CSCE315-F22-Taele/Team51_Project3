fe20a0828ed511ecd78fcb2267a7ef7f82e674e3
Johnny Le <jwonnyleaf@gmail.com>
2022-11-22 15:25:46 -0600

    Worker and user now diff in UI and minor bug fixes

--- backend/controllers/menuManagerController.js
+++ backend/controllers/menuManagerController.js
@@ -2,37 +2,35 @@ const pool = require("../server/db");
 const queries = require("../queries/queries");
 const { response } = require("express");
 
-
 const getMenuItems = (req, res) => {
-    pool.query(queries.getMenuItems, (error, results) => {
+    pool.query("SELECT * FROM menu", (error, results) => {
         if (error) throw error;
-        res.status(200).json(results.rows)
+        res.status(200).json(results.rows);
     });
 };
-
 
 const addMenuItem = (req, res) => {
     const { category, name, price, ingredients, png, options, id } = req.body;
     //add ingredient to db
-    pool.query(queries.addMenuItem, [category, name, price, ingredients, png, options, id], (error, results) => {
-        if (error) throw error;
-        res.status(201).send("Menu Item Created Successfully!");
-    });
+    pool.query(
+        "INSERT INTO menu (category, name, price, ingredients, png, options, id) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        [category, name, price, ingredients, png, options, id],
+        (error, results) => {
+            if (error) throw error;
+            res.status(201).send("Menu Item Created Successfully!");
+        }
+    );
 };
-
-
-
 
 const removeMenuItem = (req, res) => {
     //  const id  = parseInt(req.params.id);
-    const { idRemove }  = req.body;
+    const { idRemove } = req.body;
 
     pool.query("DELETE FROM menu WHERE id = $1", [idRemove], (error, results) => {
         if (error) throw error;
         res.status(200).send("Menu Item removed successfully.");
     });
 };
-
 
 module.exports = {
     getMenuItems,
--- frontend/src/components/cart/Product.js
+++ frontend/src/components/cart/Product.js
@@ -1,11 +1,19 @@
 import React from "react";
-import "./index.css"
+import { useSelector } from "react-redux";
+import "./index.css";
 
 export default function Product(props) {
     const { product, onAdd } = props;
+    const { isAuth, type } = useSelector((state) => state.auth);
+
     return (
         <div>
-            <img className="small" src={`./posPhotos/${product.png}`} alt={product.name}></img>
+            {type["type"] === "user" ? (
+                <img
+                    className="small"
+                    src={`./posPhotos/${product.png}`}
+                    alt={product.name}></img>
+            ) : null}
             <h3>{product.name}</h3>
             <div>${product.price}</div>
             <div>

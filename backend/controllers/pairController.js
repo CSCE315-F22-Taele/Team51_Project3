25fa364cc7175be7a0a9f52ab8eb9e2913588786
Mohnish Pai <86034610+mpai17@users.noreply.github.com>
2022-11-21 22:14:52 -0600

    Pair Report

--- backend/controllers/pairController.js
+++ backend/controllers/pairController.js
@@ -53,7 +53,7 @@ const getPair = (req, res) => {
 //     const secondDate = req.params.secondDate;
     pool.query(
         // "CREATE TABLE X (pid int, count int); INSERT INTO X (pid, count) SELECT productid, COUNT(*) FROM orderinfo WHERE orderid IN (SELECT orderid FROM orders WHERE Date BETWEEN '2022-11-04' AND '2022-11-05') GROUP BY productid ORDER BY count DESC; SELECT menu.name FROM menu JOIN X ON menu.id = X.pid; DROP TABLE X;",
-        "SELECT menu.name FROM menu JOIN X ON menu.id = X.pid",
+        "SELECT menu.name FROM menu JOIN X ON menu.id = X.pid ORDER BY menu.id",
         // [firstDate, secondDate],
         (error, results) => {
             if (error) throw error;

f284ca0701dd4a9e9c6c3f069f7e5a363ba6216b
Johnny Le <jwonnyleaf@gmail.com>
2022-12-04 00:08:01 -0600

    Fix a bug where refreshing stops identifying user type

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import authSlice from "./slices/authSlice";
 
-export const store = configureStore({
-    reducer: {
-        auth: authSlice,
-    },
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const reducer = combineReducers({
+    auth: authSlice,
 });
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+});
+export const persistor = persistStore(store);

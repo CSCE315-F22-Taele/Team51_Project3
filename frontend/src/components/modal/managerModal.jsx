ce641cca5e5db06e4d1fa051fd72e7bde857568b
Johnny Le <jwonnyleaf@gmail.com>
2022-12-04 20:03:52 -0600

    Manager Login from Worker

--- frontend/src/components/modal/managerModal.jsx
+++ frontend/src/components/modal/managerModal.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { useState } from "react";
 
-const ManagerModal = () => {
-    return (<div>
-        <h1>THIS IS THE MANAGER MODAL</h1>
-    </div>);
+const ManagerModal = ({ verifyManager }) => {
+    const [values, setValues] = useState({
+        userid: "78629",
+    });
+    const [message, setMessage] = useState(false);
+
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = () => {
+        fetch("api/get-managers").then((response) => {
+            response.json().then((data) => {
+                data["managers"].map((id) => {
+                    if (JSON.stringify(id) === JSON.stringify(values)) {
+                        verifyManager(true);
+                        return;
+                    }
+                });
+            });
+        });
+        
+    };
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                handleSubmit();
+            }}
+            className="form__container">
+            <h1 className="form__header">MANAGER LOGIN</h1>
+            <input
+                className="form--input"
+                type="password"
+                autoComplete="off"
+                onChange={(e) => onChange(e)}
+                value={values.password}
+                name="userid"
+                required
+            />
+            <button type="submit" className="button submit">
+                LOGIN
+            </button>
+            <div className="description status-message">{message}</div>
+        </form>
+    );
 };
 
-export default ManagerModal
+export default ManagerModal;
--- frontend/src/pages/ManagerMenu/ManagerMenu.jsx
+++ frontend/src/pages/ManagerMenu/ManagerMenu.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ManagerModal from "../../components/modal/managerModal";
 import "./managerMenu.css";
 
 export default function ManagerMenu() {
+    const { type } = useSelector((state) => state.auth);
+    const userType = type["type"];
+    const [verified, setVerified] = useState(false);
     const navigate = useNavigate();
+
+    //
+    const verifyManager = (status) => {
+        if (userType === "manager" || status) {
+            setVerified(true);
+        }
+    };
+
+    useEffect(() => {
+        verifyManager(false);
+    }, [])
+
     return (
         <div id="manager-menu">
             <div className="back__container">
@@ -17,58 +34,64 @@ export default function ManagerMenu() {
                         alt="back"></img>
                 </button>
             </div>
-            <div className="manager-menu-buttons">
-                <button>
-                    <img
+            {verified ? (
+                <div className="manager-menu-buttons">
+                    <button>
+                        <img
+                            onClick={() => {
+                                navigate("/Inventory");
+                            }}
+                            src={require("../../images/inventory.png")}
+                            alt="Manage Inventory"></img>
+                        <p>Manage Inventory</p>
+                    </button>
+                    <button
                         onClick={() => {
-                            navigate("/Inventory");
-                        }}
-                        src={require("../../images/inventory.png")}
-                        alt="Manage Inventory"></img>
-                    <p>Manage Inventory</p>
-                </button>
-                <button
-                    onClick={() => {
-                        navigate("/OrderHistory");
-                    }}>
-                    <img
-                        className="statButton"
-                        src={require("../../images/orderhistory.png")}
-                        alt="Order History"></img>
-                    <p>Order History</p>
-                </button>
-                <button onClick={() => navigate("/Excess")}>
-                    <img
-                        src={require("../../images/excess.png")}
-                        alt="Excess Report"></img>
-                    <p>Excess Report</p>
-                </button>
-                <button
-                    onClick={() => {
-                        navigate("/Pair");
-                    }}>
-                    <img src={require("../../images/pair.png")} alt="Pair Report"></img>
-                    <p>Pair Report</p>
-                </button>
-                <button
-                    onClick={() => {
-                        navigate("/Revenue");
-                    }}>
-                    <img
-                        src={require("../../images/revenue.png")}
-                        alt="Revenue Report"></img>
-                    <p>Revenue</p>
-                </button>
-                <button
-                    onClick={() => {
-                        navigate("/EditMenu");
-                    }}>
-                    <img
-                        src={require("../../images/menufunctions.png")}
-                        alt="Menu Functions"></img>
-                    <p>Menu Functions</p>
-                </button>
-            </div>
+                            navigate("/OrderHistory");
+                        }}>
+                        <img
+                            className="statButton"
+                            src={require("../../images/orderhistory.png")}
+                            alt="Order History"></img>
+                        <p>Order History</p>
+                    </button>
+                    <button onClick={() => navigate("/Excess")}>
+                        <img
+                            src={require("../../images/excess.png")}
+                            alt="Excess Report"></img>
+                        <p>Excess Report</p>
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigate("/Pair");
+                        }}>
+                        <img
+                            src={require("../../images/pair.png")}
+                            alt="Pair Report"></img>
+                        <p>Pair Report</p>
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigate("/Revenue");
+                        }}>
+                        <img
+                            src={require("../../images/revenue.png")}
+                            alt="Revenue Report"></img>
+                        <p>Revenue</p>
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigate("/EditMenu");
+                        }}>
+                        <img
+                            src={require("../../images/menufunctions.png")}
+                            alt="Menu Functions"></img>
+                        <p>Menu Functions</p>
+                    </button>
+                </div>
+            ) : (
+                <ManagerModal verifyManager={verifyManager}></ManagerModal>
+            )}
         </div>
     );
 }

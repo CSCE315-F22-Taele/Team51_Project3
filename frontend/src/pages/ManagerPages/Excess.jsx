342d6bdcd03417a1a2f0d2e17bb63667cdb058dd
Johnny Le <jwonnyleaf@gmail.com>
2022-12-06 02:25:31 -0600

    Excess Styling and Bugs Fixed

--- frontend/src/pages/ManagerPages/Excess.jsx
+++ frontend/src/pages/ManagerPages/Excess.jsx
@@ -6,12 +6,11 @@ import backbutton from "../../images/backbutton.png";
 export default function Excess() {
     const [firstDate, setStartDate] = useState("");
     const [secondDate, setEndDate] = useState("");
-    const [ingredientsID, setIngredientsID] = useState();
+    const [ingredientsID, setIngredientsID] = useState([]);
     const [usageCount, setUsageCount] = useState([]);
     const [inventory, setInventory] = useState([]);
     const [initialInventory, setInitialInventory] = useState([]);
     const [excessItems, setExcessItems] = useState([]);
-    const [displayData, setDisplayData] = useState([]);
 
     const didMount = useRef(false);
     const didMount2 = useRef(false);
@@ -31,14 +30,6 @@ export default function Excess() {
             checkExcessGivenDate();
         }
     }, [usageCount]);
-
-    useEffect(() => {
-        if (!didMount2.current) {
-            didMount2.current = true;
-        } else {
-            setDisplayData(setTableView());
-        }
-    }, [excessItems]);
 
     /**
      * Fetches the information from Database Table: 'Ingredients' that was sent to resource (HTTP)
@@ -61,6 +52,7 @@ export default function Excess() {
     }
 
     const getIngredientUsage = async (event) => {
+        console.log(firstDate, secondDate);
         event.preventDefault();
         try {
             await fetch("api/excess/ingredientusage", {
@@ -108,7 +100,9 @@ export default function Excess() {
         } catch (err) {
             console.error(err);
         }
+    }
 
+    useEffect(() => {
         const excessed = inventory.filter(function (item) {
             try {
                 if (usageCount[item.id] < initialInventory[0][item.id] * 0.1) {
@@ -120,92 +114,95 @@ export default function Excess() {
             return false;
         });
         setExcessItems(excessed);
-    }
+    }, [initialInventory]);
 
-    function setTableView() {
-        const displayInfo = excessItems.map((item) => {
-            return (
-                <tr>
-                    <td style={{ fontSize: `${fontSize}px` }}>{item.id}</td>
-                    <td style={{ fontSize: `${fontSize}px` }}>{item.name}</td>
-                    <td style={{ fontSize: `${fontSize}px` }}>
-                        {Number(
-                            usageCount[item.id] / parseFloat(initialInventory[0][item.id])
-                        ).toLocaleString(undefined, {
-                            style: "percent",
-                            minimumFractionDigits: 2,
-                        })}
-                    </td>
-                </tr>
-            );
-        });
-        return displayInfo;
-    }
+    console.log(usageCount, initialInventory);
+    const displayInfo = excessItems.map((item) => {
+        if (initialInventory.length < 1 || initialInventory === undefined) {
+            return;
+        }
+        return (
+            <tr>
+                <td style={{ fontSize: `${fontSize}px` }}>{item.id}</td>
+                <td style={{ fontSize: `${fontSize}px` }}>{item.name}</td>
+                <td style={{ fontSize: `${fontSize}px` }}>
+                    {Number(
+                        usageCount[item.id] / parseFloat(initialInventory[0][item.id])
+                    ).toLocaleString("en-US", {
+                        style: "percent",
+                        minimumFractionDigits: 2,
+                    })}
+                </td>
+            </tr>
+        );
+    });
 
     return (
-        <div className="App">
-            <tr>
-                <th>
-                    {" "}
-                    <div>
-                        <button>
-                            <img
-                                onClick={() => {
-                                    navigate("/ManagerMenu");
-                                }}
-                                className="backbutton"
-                                src={backbutton}
-                                alt="back"
-                            ></img>
-                        </button>
-                    </div>{" "}
-                </th>
-                <th>
-                    <button onClick={() => setFontSize(fontSize + 2)}>
-                        + increase font size
-                    </button>{" "}
-                </th>
-                <th>
-                    {" "}
-                    <button onClick={() => setFontSize(fontSize - 2)}>
-                        - decrease font size
-                    </button>{" "}
-                </th>
-            </tr>
-            <h1>Excess Report </h1>
-            <form
-                onSubmit={(event) => {
-                    getIngredientUsage(event);
-                }}
-            >
-                <input
-                    type="string"
-                    placeholder="yyyy-mm-dd"
-                    onChange={(event) => {
-                        setStartDate(event.target.value);
-                    }}
-                    required
-                ></input>
-                <input
-                    type="string"
-                    placeholder="yyyy-mm-dd"
-                    onChange={(event) => {
-                        setEndDate(event.target.value);
-                    }}
-                    required
-                ></input>
-                <button>Submit</button>
-            </form>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th style={{ fontSize: `${fontSize}px` }}>Item ID</th>
-                        <th style={{ fontSize: `${fontSize}px` }}>Item Name</th>
-                        <th style={{ fontSize: `${fontSize}px` }}>Percentage Sold</th>
-                    </tr>
-                </thead>
-                <tbody>{displayData}</tbody>
-            </table>
+        <div className="excess-page manager-page">
+            <div className="excess-topbar">
+                <div className="back__container">
+                    <button
+                        onClick={() => {
+                            navigate("/managermenu");
+                        }}
+                    >
+                        <img
+                            className="backbutton"
+                            src={require("../../images/backbutton.png")}
+                            alt="back"
+                        ></img>
+                    </button>
+                </div>
+                <div className="excess-actions">
+                    <form
+                        id="excess-form"
+                        className="excess-actions__container"
+                        onSubmit={(event) => {
+                            getIngredientUsage(event);
+                        }}
+                    >
+                        <input
+                            className="excess-actions--input"
+                            type="date"
+                            placeholder="yyyy-mm-dd"
+                            onChange={(event) => {
+                                setStartDate(event.target.value);
+                            }}
+                            required
+                        />
+                        <input
+                            className="excess-actions--input"
+                            type="date"
+                            placeholder="yyyy-mm-dd"
+                            onChange={(event) => {
+                                setEndDate(event.target.value);
+                            }}
+                            required
+                        />
+                        <button className="button">Search</button>
+                    </form>
+                </div>
+                <div className="excess-accessibility">
+                    <button className="button" onClick={() => setFontSize(fontSize + 2)}>
+                        + Font Size
+                    </button>
+                    <button className="button" onClick={() => setFontSize(fontSize - 2)}>
+                        - Font Size
+                    </button>
+                </div>
+            </div>
+            <div className="excess-table">
+                <table className="styled-table">
+                    <thead>
+                        <tr>
+                            <th style={{ fontSize: `${fontSize}px` }}>Item ID</th>
+                            <th style={{ fontSize: `${fontSize}px` }}>Item Name</th>
+                            <th style={{ fontSize: `${fontSize}px` }}>Percentage Sold</th>
+                        </tr>
+                    </thead>
+                    <tbody>{displayInfo}</tbody>
+                </table>
+            </div>
         </div>
     );
 }
